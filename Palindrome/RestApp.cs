bedcdbc707a2b3bd125376c16bca27dbd598a5c8
John Dominguez <john.dominguez@stonybrook.edu>
2018-04-23 11:57:30 -0400

    Commence Project0 (version 3)

---
+++ Palindrome/RestApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class RestApp
+    {
+    }
+}
+
+
+/*
+    Project 0
+    -------------------
+    Restaurant - Console
+    Restaurant 
+    -name
+    - +2 info : loc, desc
+    Review
+    -numerical rating
+    -2 info: reviewer name, date
+*/
